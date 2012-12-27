226eef3261e9714e9fd31aedcb577d31715ad765
Jacek Podkanski <ruby.object@googlemail.com>
2012-12-27 01:42:43 +0000

    created new project and added gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Palakura::Application.config.secret_token = '0accf688976fd2c4cd6480e2152c371d17921418b282d64fab9e5fe30cde8a55cf9f1cc2744b9b90eea3a943b9ec6159fce0f4ccd5e477b6e4f8093512ffaca8'
